c5a5dff89612fff017a13023b55c3e6ab7f4e2d6
saharsamr <sahar.rajabi76@gmail.com>
2018-07-09 17:14:29 +0430

    last commit

--- Coloring/ColouringGraph.r
+++ Coloring/ColouringGraph.r
@@ -1,4 +1,5 @@
 library(igraph)
+library(lpSolve)
 
 NO_COLOR = -1
 
@@ -24,5 +25,64 @@ greedy_coloring <- function(graph) {
   print(max(V(graph)$color_num))
 }
 
+integer_programing <- function(graph) {
+  size = length(V(graph))
+  coefficients = rep(1, size)
+  coefficients = append(coefficients, rep(0, size*size))
+  num_of_variables = size + size*size
+
+  relations = c()
+  margins = c()
+  constraints <- matrix(ncol = num_of_variables)
+
+  for (i in 1:num_of_variables) {
+    constraint = rep(0, num_of_variables)
+    constraint[i] = 1
+    rbind(constraints, rep(1, num_of_variables)) # variables >= 0 constraint
+    relations = append(relations, ">=")
+    margins = append(margins, 0)
+    constraint = rep(0, num_of_variables)
+    constraint[i] = 1
+    rbind(constraints, rep(1, num_of_variables)) # variables <= 1 constraint
+    relations = append(relations, "<=")
+    margins = append(margins, 1)
+  }
+
+  for (i in 1:size) # vertex <-> color
+    for (j in 1:size) { # color
+      constraint <- rep(0, num_of_variables)
+      constraint[j] = -1
+      constraint[size+(i-1)*size+j] = 1
+      rbind(constraints, constraint) # x(ik) - y(k) <= 0 constraint
+      relations = append(relations, "<=")
+      margins = append(margins, 0)
+    }
+
+  for (i in 1:size) {
+    constraint <- rep(0, num_of_variables)
+    constraint[size+(i-1)*size+1:size+(i-1)*size+size] = rep(1, size)
+    rbind(constraints, constraint) # sigma(x(ik)) = 1 constraint
+    relations = append(relations, "=")
+    margins = append(margins, 1)
+  }
+
+  for (edge in E(graph)) {
+    incidents = ends(graph, edge, names = FALSE)
+    for (j in 1:size) {
+      constraint = rep(0, num_of_variables)
+      constraint[size+(incidents[1,1]-1)*size+j] = 1
+      constraint[size+(incidents[1,2]-1)*size+j] = 1
+      rbind(constraints, constraint) # x(ik) + x(jk) <= 1 constraint
+      relations = append(relations, "<=")
+      margins = append(margins, 1)
+    }
+  }
+
+  lp = lp(direction="min", objective.in=coefficients, const.mat=constraints, const.dir=relations, const.rhs=margins,
+        transpose.constraints=TRUE, int.vec=1:num_of_variables)
+  print(lp)
+}
+
 graph = sample_gnm(50, 500)
 greedy_coloring(graph)
+integer_programing(graph)
